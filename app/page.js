f8de2116097fa63b3becf15cb7b0d47c6d5ae1b8
sifuna2002 <sifunadone4704@gmail.com>
2023-06-20 21:30:48 +0300

    Nav

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { getSession,useSession } from 'next-auth/react'
 import Login from '@/components/Login'
+import Nav from '@/components/Nav'
 
 export default function Home() {
   const { data: session, status } = useSession()
@@ -10,9 +11,7 @@ export default function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button className='bg-teal-500 rounded-md p-7'>Welcome Home</button>  
-      </div>
+      <Nav />
     </main>
   )
 }
